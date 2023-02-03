fe4752ce42e1d441d726878b26b3473004f6bc9b
arpitjaswal-integrella <arpit.jaswal@integrella.com>
2023-02-03 11:23:58 +0530

    DrumKit completed. Concepts learnt: eventListeners, selectors, DOM manipulations, animations

--- index.js
+++ index.js
@@ -1,5 +1,105 @@
-document.querySelector("button").addEventListener("click", igotclicked);
+var length = document.querySelectorAll(".drum").length;
 
-function igotclicked(){
-    alert("I got clicked!");
+for(var i = 0; i<length ; i++){
+    var inn = this.innerHTML;
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var inn = this.innerHTML;
+        makeSound(inn);
+        buttonAnimation(inn);
+    });
 }
+
+//key press
+document.addEventListener("keydown", (event)=>{
+    makeSound(event.key);
+    buttonAnimation(event.key)
+})
+
+function makeSound(inn){
+    switch (inn) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+    
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+
+document.addEventListener("keydown", (e)=>{
+    var inn = e.code;
+
+    switch (inn) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+    
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+    
+        default:
+            break;
+    }
+})
+
+function buttonAnimation(key){
+    var activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 300);
+
+}
+
+// var audio = new Audio("sounds/tom-3.mp3");
+//     audio.play();
